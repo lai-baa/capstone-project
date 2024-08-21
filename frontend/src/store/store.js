0726dfa00532c442256e4fe8a4325d4602fe9c8e
Laiba Afzal <laiba.a1298@gmail.com>
2024-08-21 14:15:21 -0400

    redux store setup

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,18 +1,17 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+// frontend/src/store/store.js
 
-import sessionReducer from "./session";
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
-  session: sessionReducer,
+
 });
 
 let enhancer;
-
-if (process.env.NODE_ENV === "production") {
+if (import.meta.env.MODE === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require("redux-logger").default;
+  const logger = (await import("redux-logger")).default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
