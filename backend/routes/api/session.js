ad7d97818ca67854a1fd8b33587283e8b79f4498
Laiba Afzal <laiba.a1298@gmail.com>
2024-08-21 12:21:57 -0400

    added session actions and reducer

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -52,6 +52,8 @@ router.post(
       lastName: user.lastName,
       email: user.email,
       username: user.username,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
     };
 
     await setTokenCookie(res, safeUser);
@@ -83,6 +85,8 @@ router.get(
         lastName: user.lastName,
         email: user.email,
         username: user.username,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt  
       };
       return res.json({
         user: safeUser
