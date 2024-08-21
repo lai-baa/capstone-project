d433f6dabf7b4771c0c8da95450be6663aee8047
Laiba Afzal <laiba.a1298@gmail.com>
2024-08-21 15:55:29 -0400

    finishing up modal components and testing nav bar

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -4,14 +4,18 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import { GiNotebook } from "react-icons/gi";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
     <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
+      <li id="nav-link-home">
+        <NavLink to="/">
+            <div id="site-icon"><GiNotebook /></div>
+            KayKew
+        </NavLink>
       </li>
       {isLoaded && (
         <li>
