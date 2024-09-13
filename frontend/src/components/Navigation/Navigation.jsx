2cc3dca5db0174bc7068826f4e897f04ec3f6110
Laiba Afzal <laiba.a1298@gmail.com>
2024-09-12 22:43:37 -0400

    refactored nav bar

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,12 +1,10 @@
-// frontend/src/components/Navigation/Navigation.jsx
-
 import { NavLink, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector(state => state.session.user);
+  const sessionUser = useSelector(state => state.session.user); // Check if the user is logged in
 
   return (
     <ul className='nav-links-container'>
@@ -15,14 +13,16 @@ function Navigation({ isLoaded }) {
         <img src="/favicon.ico" alt="KayKewNotes" className="nav_icon" />
         </NavLink>
       </li>
-      
-      <div className='nav-links'>
+
+      {sessionUser && ( // Only render these links when the user is logged in
+        <div className='nav-links'>
           <Link to="/notebooks" className="nav-link">All Notebooks</Link>
           <Link to="/tasks" className="nav-link">Pending Tasks</Link>
           <Link to="/reminders" className="nav-link">Reminders</Link>
           <Link to="/favorites" className="nav-link">Favorites</Link>
           <Link to="/tasks/completed" className="nav-link">Completed Tasks</Link>
-      </div>
+        </div>
+      )}
 
       {isLoaded && (
         <li>
