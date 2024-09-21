ccf3a0a15c19f4fe27fe388ea32cde509301b50d
Laiba Afzal <laiba.a1298@gmail.com>
2024-09-20 21:50:39 -0400

    fixing build errors

--- frontend/src/components/FilteredNotes/FilteredNotes.jsx
+++ frontend/src/components/FilteredNotes/FilteredNotes.jsx
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import "./FilteredNotes.css";
